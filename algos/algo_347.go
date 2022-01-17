821d16bd511a571785f724109195b5a71c03c242
keithyin <yinpenghhz@hotmail.com>
2022-01-17 23:45:57 +0800

    'algo'

--- algos/algo_347.go
+++ algos/algo_347.go
@@ -2,4 +2,14 @@ package algos
 
 func topKFrequent(nums []int, k int) []int {
 
+	counter := make(map[int]int, 0)
+	for _, v := range nums {
+		if _, ok := counter[v]; !ok {
+			counter[v] = 0
+		}
+		counter[v] += 1
+	}
+
+	return []int{1}
+
 }
